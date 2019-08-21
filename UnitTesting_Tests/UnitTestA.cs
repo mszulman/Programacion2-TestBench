83c9e13d54e3fe58198b10d552c074099c6269fa
Matias Szulman <mszulman@ort.edu.ar>
2019-08-21 19:52:06 -0300

    .

--- UnitTesting_Tests/UnitTestA.cs
+++ UnitTesting_Tests/UnitTestA.cs
@@ -11,14 +11,14 @@ namespace UnitTesting_Tests
         public void TestMethodA()
         {
             var a = new UnitTesting_Code.ClassA();
-            Assert.AreEqual(a.DeciHola(), "Hi");
+            Assert.AreEqual("Hola", a.DeciHola());
         }
 
-        //[TestMethod]
-        //public void TestMethodB()
-        //{
-        //    var a = new UnitTesting_Code.ClassA();
-        //    a.DeciHola().Should().StartWith("Hi");
-        //}
+        [TestMethod]
+        public void TestMethodB()
+        {
+            var a = new UnitTesting_Code.ClassA();
+            a.DeciHola().Should().Be("Hola");
+        }
     }
 }
